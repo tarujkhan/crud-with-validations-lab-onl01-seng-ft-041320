17a3174a51ace166a67db635c2bd0cdde8541f94
taruj khan <tarujkhan@DESKTOP-E8JDKB5.localdomain>
2020-06-17 22:05:48 -0500

    This lab is done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :songs, only: [:index, :new, :create, :show, :edit, :update]
+  delete '/songs/:id', to: "songs#destroy" 
+  root "songs#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
